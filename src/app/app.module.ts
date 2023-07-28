ba6fcd7224ef37fbfe69bad3a2d6588dc8006a74
Antoine Lardant <antoine.lardant@gmail.com>
2023-07-28 19:31:19 +0200

    add tags bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchComponent } from './search/search.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { TagsComponent } from './tags/tags.component'
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms'
     HeaderComponent,
     HomeComponent,
     SearchComponent,
+    TagsComponent,
   ],
   imports: [
     BrowserModule,
