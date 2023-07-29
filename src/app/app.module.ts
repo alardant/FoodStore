bd56c16f66ff24347b9a59e48dce4bd1fbfee3a3
Antoine Lardant <antoine.lardant@gmail.com>
2023-07-29 14:21:26 +0200

    add not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
 import { FoodPageComponent } from './food-page/food-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component'
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { CartPageComponent } from './cart-page/cart-page.component'
     TagsComponent,
     FoodPageComponent,
     CartPageComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
